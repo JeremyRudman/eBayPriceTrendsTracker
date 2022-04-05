7972ad62db4f04d6f6517ca56798296ca2ba22e3
Jeremy Rudman <JeremyRudman@gmail.com>
2022-04-05 16:51:47 -0400

    minor tweaks to product info

--- src/ProductInfo.js
+++ src/ProductInfo.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import './ProductInfo.css';
 import { withParams } from './withParams'
+import * as d3 from "d3";
+
 
 
 class ProductInfo extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            item_pricing: []
+        }
+    }
 
 
     componentDidMount() {
         const {name} = this.props.params
         console.log(this.props.params)
         console.log(name)
-        // fetch(`https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=${this.props.match.params.name}&limit=100`).then(response => response.json())
-        // .then(data => {}).catch(err => console.log(err));
+        const header = new Headers();
+        header.append('Content-Type', 'application/json');
+        header.append('Authorization', '');
+        fetch(`https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=${name}&limit=100`,
+        {method: 'GET',
+        headers: header})
+        .then(response => response.json())
+        .then(data => {console.log(data)}).catch(err => console.log(err));
     }
 
     render() {
-        return (
-            <div className='product_body'>
-                test
-            </div>
-        )
+        if(this.state.item_pricing.length === 0){
+            return(
+                <div>Loading</div>
+            )
+        } else {
+            return (
+                <div className='product_body'>
+                    
+                </div>
+            )
+        }
     }
 }
 
